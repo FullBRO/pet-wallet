3fc4084a979c6e3fd59d8091ee0e9769836dede6
FullBRO <jenya.radchenko@gmail.com>
2026-02-16 08:46:31 +0200

    minor code cleanup

--- src/modules/events/helper.ts
+++ src/modules/events/helper.ts
@@ -6,27 +6,36 @@ import { EVENT_STATUSES, PostEventData } from "./constants.js"
 export async function createEvent(data: PostEventData, transaction: Transaction): Promise<Event | null>{
     try{
 
-        const event = await Event.create(
-            {
-                source: data.source, 
-                type: data.type,
-                occurred_at: data.timestamp,
-                event_uid: data.id,
-            }, 
-            {
-                transaction
-            }
-        )
+        const event = await Event.create({
+            source: data.source, 
+            type: data.type,
+            occurred_at: data.timestamp,
+            event_uid: data.id,
+        }, 
+        {transaction})
+
         await Payload.create({
             id: event.id,
             payload: JSON.stringify(data.data)
         },
         {transaction})
-        await Event.update({status: EVENT_STATUSES.processing}, {where: {id: event.id, status: EVENT_STATUSES.received }, transaction})
-        transaction.afterCommit(()=>{
-            postEventQueue.add('process',{eventId: event.id}, { jobId: `event_${event.id.toString()}`})
-                .catch(error => console.error(error))
+
+        await Event.update({
+            status: EVENT_STATUSES.processing
+        }, {
+            where: {
+                id: event.id, 
+                status: EVENT_STATUSES.received
+            },
+            transaction
         })
+        
+        transaction.afterCommit(()=>{
+            postEventQueue
+            .add('process',{eventId: event.id}, { jobId: `event_${event.id.toString()}`})
+            .catch(error => console.error(error))
+        })
+        
         return event
     }catch(error){
         if(error instanceof UniqueConstraintError){
